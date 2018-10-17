ea0626b3935273ee571053188876cc8dd41d6d4c
fauzia <fauziaazia171097@gmail.com>
2018-10-17 17:21:24 +0700

    updated

---
+++ cari pelaku.js
@@ -0,0 +1,16 @@
+// Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. Gunakan regex untuk melatih kemampuan regex.
+
+// Code
+function cariPelaku(str) {
+
+    return str.match(/abc/m)counter++;
+    console.log(str)
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
---
+++ hapus symbol.js
@@ -0,0 +1,16 @@
+// Diberikan sebuah function hapusSimbol(str) yang menerima satu parameter berupa string. Function akan me-return string yang telah bersih dari berbagai simbol, hanya menyisakan a-z dan angka 0-9.
+//console.log(/[A-Z]/.test(message))
+// Code
+function hapusSimbol(str) {
+  
+  return str.match(/[a-z,0-9]/g).join('');
+  
+  
+}
+
+// TEST CASES
+console.log(hapusSimbol('test%$4aa')); // test4aa
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
